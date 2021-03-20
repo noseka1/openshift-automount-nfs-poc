be13dac865415e8687951e90c7727f2104a693cf
Ales Nosek <ales.nosek@gmail.com>
2021-03-20 13:51:04 -0700

    Configure automount using configmap

--- automount/run.sh
+++ automount/run.sh
@@ -8,15 +8,6 @@ dnf install --assumeyes \
 
 mkdir -p /host/var/mnt/automount
 
-# Create automount configuration
-cat >/etc/auto.master.d/extra.autofs <<EOF
-/host/var/mnt/automount /etc/extra.nfs
-EOF
-# REPLACE with the your NFS server IP address
-cat >/etc/extra.nfs <<EOF
-* -rw 10.129.2.4:/exports/&
-EOF
-
 # Start the autofs
 exec automount \
   --foreground \
