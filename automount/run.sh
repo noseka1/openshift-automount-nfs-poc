1984c355d87d20c3a5d870ee48ce46abdb6b51af
Ales Nosek <ales.nosek@gmail.com>
2021-03-20 13:40:53 -0700

    Use here document for better readability

--- automount/run.sh
+++ automount/run.sh
@@ -9,8 +9,13 @@ dnf install --assumeyes \
 mkdir -p /host/var/mnt/automount
 
 # Create automount configuration
-echo '/host/var/mnt/automount /etc/extra.nfs' > /etc/auto.master.d/extra.autofs
-echo '* -rw 10.129.2.4:/exports/&' > /etc/extra.nfs
+cat >/etc/auto.master.d/extra.autofs <<EOF
+/host/var/mnt/automount /etc/extra.nfs
+EOF
+# REPLACE with the your NFS server IP address
+cat >/etc/extra.nfs <<EOF
+* -rw 10.129.2.4:/exports/&
+EOF
 
 # Start the autofs
 exec automount \
