57e36d6ed5aee3e175f8a709089475af75bbe92b
Ales Nosek <ales.nosek@gmail.com>
2021-03-19 18:32:36 -0700

    Fix nfs mount hanging part II
    Cannot have multiple fsid values for the same file system

--- nfs-server/init.sh
+++ nfs-server/init.sh
@@ -12,21 +12,21 @@ mount -t nfsd nfds /proc/fs/nfsd
 /usr/sbin/rpc.nfsd -G 10 -N 2 -V 3
 /usr/sbin/rpc.statd --no-notify
 
-# Create exports
-I=1000
+# Create directories
 for CITY in dallas tucson sandiego; do
   # Create exported directory if it doesn't exit
   mkdir -p /exports/$CITY
   # allow wide open access
   chmod 777 /exports/$CITY
-  # Add the directory to the list of exported dirs
-  echo "/exports/$CITY *(rw,fsid=$I,async,root_squash)" > /etc/exports.d/$CITY.exports
-  # Each export must have a unique fsid
-  ((I++))
+  # create a test file
+  echo $CITY > /exports/$CITY/$CITY.txt
 done
 
+# Add the exports directory to the list of exported dirs
+echo "/exports *(rw,fsid=0,async,root_squash)" > /etc/exports.d/city.exports
+
 # Export city directory via nfs
-exportfs -a
+exportfs -r
 
 # Show current nfs exports
 showmount -e
